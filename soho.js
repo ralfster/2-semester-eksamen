e58ea084eb160fc3342df3c615bf471e87f3f97b
Ralf Dalby <32537467+ralfster@users.noreply.github.com>
2018-05-30 08:46:12 +0200

    Add files via upload

---
+++ soho.js
@@ -0,0 +1,70 @@
+document.getElementById("readmore1").onclick = function () {
+    foldUd()
+} 
+function foldUd() {
+    console.log("yeds");
+    
+    document
+    .getElementById("historietekst")
+    .innerHTML = "- Indehaverne Hanne Møldrup Jensen og Hanne Møller fik arbejde hos Sophies tøjhus, da deres arbejdsliv startede. Ansættelsen udviklede sig til noget dybere for dem, end bare at sælge tøj. Sophies tøjhus havde en stor indflydelse på Hanne og Hanne i forhold til deres syn på modebranchen. Der blev taget chancer i Sophies tøjhus. Sophies tøjhus solgte både kvinde- og herretøj, men havde et smalt sortiment inden for herretøj. Det smalle sortiment af herretøj indeholdt arbejdstøj, mere specifikt kinaskjorter. Sophies tøjhus fandt tøjet stilløst, og ville prøve at udfordre stilen, ved at farve tøjet. Dette var en stor succes, og tøjet var eftertragtet af kunder. Kundesegmentet inden for herretøj voksede, og Sophies tøjhus så nøden til at udvide, så de kunne tilfredsstille deres kunder. SOHO blev grundlagt. Hanne og Hanne blev de ansvarsfulde for forretningen, hvor de skulle sælge herretøjet. Hanne og Hanne nød denne udvikling, og de havde nu ansvaret. Passionen voksede og herretøjet der blev solgt i SOHO var anderledes, end det man kunne finde andre steder i Aarhus. Efter en lang årrække fik Hanne og Hanne muligheden for at overtage SOHO fra Sophies tøjhus. De havde styret SOHO op til nu, og nød arbejdet inden for herretøjsbranchen. De havde bygget en unik forretning op, og havde været en del af at skabe noget nyt og anderledes inden for herremoden i Aarhus. De takkede ja til muligheden, og blev ved med at føre deres visioner frem igennem butikken. Disse visioner og tilgang til at føre forretning ses tydeligt i SOHO i dag.";
+
+}
+   
+/*---------------------------------------------------------------------------------*/
+document.getElementById("readmore2").onclick = function () {
+    foldUd
+
+    
+    document
+    .getElementById("tekst3")
+    .innerHTML = "- SOHO vil være anderledes! At skille sig ud fra mængden og tilbyde ens kunder noget unikt i form af produkter og oplevelser er drivkraften bag SOHO. SOHO tager imod dig med åbne arme, og stræber efter at gøre din oplevelse hos SOHO til noget unikt. SOHO tilbyder ”hele pakken”, i form af kvalitetsprodukter, personlig og videns rig service og et bredt sortiment til enhver smag. Ud over dette, byder SOHO dig velkommen ind i varme og trygge rammer. Forretningen er præget af en afslappende stemning, som får dig til at føle dig hjemme.";
+}
+
+/*--------------------------------------------------------------------------------------*/
+document.getElementById("readmore3").onclick = function () {
+    foldUd
+
+ 
+    document
+    .getElementById("tekst4")
+    .innerHTML = 'Den græske filosof, Aristoteles sagde en engang, <i>"Art imitates nature."</i> Netop dette inspirerede Tommy, og ændrede hans perspektiv på skønhed. Denne åbenbaring banede vej for en dybere værdsættelse af selv naturens simple skønheder. <br><br> Kashmir er et naturmateriale som er let, blødt og smidigt. Derfor tilpasser ulden sig med tiden kroppens former. Som Tommy beskriver, er filosofien bag COLORCLOUDSTUDIO_KNIT tæt knyttet til kashmir, fordi de ønsker at skabe en strik der tilpasser sig hvert menneskes form. Han ønsker, at genskabe den følelse han fik tilbage i 1994 i hans strik. Derfor er sweateren ikke konstrueret af et tilfældigt materiale, men netop kashmir fordi ulden med tiden bliver en genskabelse af vedkommendes skikkelse. Pasformen som strikken med tiden får, tilføjer en stærk personlig følelse til trøjen.';
+}
+/*--------------------------------------------------------------------------------------*/
+document.getElementById("readmore4").onclick = function () {
+    foldUd
+
+ 
+    document
+    .getElementById("tekst6")
+    .innerHTML = "Det er ikke kun kvaliteten af sweateren, der har stor betydning for Tommy. Han mener, at den vigtigste del af processen er, at inddrage kunden. Forud for kreationen af sweateren, har kunden en dialog med Tommy. Dialogen er begyndelsen på den selvindsigt, der skaber hver enkelt strik. Dialogen danner ikke kun ramme for det fysiske udtryk af strikken, men skaber også den emotionelle association til trøjen.";
+}
+/*--------------------------------------------------------------------------------------*/
+document.getElementById("readmore5").onclick = function () {
+    foldUd
+
+    
+    document
+    .getElementById("tekst6ekstra")
+    .innerHTML = "Det er betydningsfuldt for Tommy, at have et stort udvalg af forskellige farver tråd. Dette er, som Tommy understreger, fordi at tråden repræsenterer temperaturen på mennesket. Mulighederne for de mange farvekombinationer af tråde, og variationer i faconen af sweateren, skaber uendelige muligheder. På den måde bliver hver kundes strik personliggjort, og den følelsesmæssige forbindelse bliver forstærket.";
+}
+/*--------------------------------------------------------------------------------------*/
+document.getElementById("readmore6").onclick = function () {
+    foldUd
+
+    document
+    .getElementById("tekst7")
+    .innerHTML = "Kødbyen er en gammel bydel af Aarhus, hvor mange kreative virksomheder holder til. Området er stille og uforstyrret med undtagelsen for havets brusen, og et par enkelte sæler der titter frem fra overfladen. Studiets kreativitet skinner tydeligt igennem i form af arbejdsmiljøet. Strikkemaskiner, tråd i adskillige farver og designskitser pryder lokalet og udgør COLORCLOUDSTUDIO_KNITs essens.";
+}
+/*--------------------------------------------------------------------------------------*/
+
+    document.getElementById("finalbanner_activate").onmouseover = function () {
+        myFunction()
+    };
+
+    function myFunction() {
+        console.log("hurra!");
+
+        document
+            .getElementById("finalbanner")
+            .innerHTML = "ART IMITATES YOU";}
+/*-----------------------------------------------------------------------------------*/
